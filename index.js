06376b3cd549c8748ab4564ca023929ee3f23ed5
Andreas Hartung <ahartung@outlook.com>
2019-08-26 01:27:52 +0200

    sequelize init

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const apiRouter = require('./backend/router/api.router');
+const db = require('./models');
 
-express()
-    .use(bodyParser({limit: '50mb'}))
-    .use(bodyParser.json())
-    .use(cors())
-    .use(express.static(path.join(__dirname, 'public')))
-    .use('/api/v1', apiRouter)
-    .get('/', (req, res) => res.render('public/index.html'))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+db.sequelize.sync().then(function() {    express()
+        .use(bodyParser({limit: '50mb'}))
+        .use(bodyParser.json())
+        .use(cors())
+        .use(express.static(path.join(__dirname, 'public')))
+        .use('/api/v1', apiRouter)
+        .get('/', (req, res) => res.render('public/index.html'))
+        .listen(PORT, () => console.log(`Listening on ${PORT}`));
+});
+
